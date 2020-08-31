d6ac02ac38d28fb3167d74e580b421d898fd7ecf
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-01 02:22:03 +0500

    added color palette

--- Client/lib/constants/colors.dart
+++ Client/lib/constants/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-const lightPrimary = Color(0xFFf5f5ff);
+class Palette {
+  static get scaffoldBackgroundColor => Colors.indigo[50];
+  static get primaryColor => Colors.indigo[900];
+  static get accentColor => Colors.purple;
+}
