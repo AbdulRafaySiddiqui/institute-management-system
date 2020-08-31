f8eb35cf354768d159a2126b8d627a0b15693dbe
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-01 02:25:43 +0500

    registered branch api

--- Client/lib/locator/locator.dart
+++ Client/lib/locator/locator.dart
@@ -1,8 +1,10 @@
 import 'package:Client/service/NavigationService/NavigationService.dart';
+import 'package:Client/service/api/BranchApi.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => BranchApi());
 }
