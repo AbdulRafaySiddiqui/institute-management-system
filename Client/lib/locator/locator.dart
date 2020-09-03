072563bdb667305a32043b572a39a242f5723137
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-03 16:47:18 +0500

    added classApi

--- Client/lib/locator/locator.dart
+++ Client/lib/locator/locator.dart
@@ -1,5 +1,6 @@
 import 'package:Client/service/NavigationService/NavigationService.dart';
 import 'package:Client/service/api/BranchApi.dart';
+import 'package:Client/service/api/ClassApi.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
@@ -7,4 +8,5 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => BranchApi());
+  locator.registerLazySingleton(() => ClassApi());
 }
