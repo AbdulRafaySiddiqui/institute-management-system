9e8f6e5904ec514d6272d6c0282841079e34ba63
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-03 16:37:24 +0500

    added base created & modified info

--- Client/lib/models/BaseModel.dart
+++ Client/lib/models/BaseModel.dart
@@ -2,7 +2,31 @@ class BaseModel {
   BaseModel({this.id});
   int id;
 
+  String createdBy;
+  DateTime dateTimeCreated;
+
+  String modifiedBy;
+  DateTime dateTimeModified;
+
   toInt(dynamic value) => value == null
       ? 0
       : value is String ? int.parse(value) : int.parse(value.toString());
+
+  BaseModel.fromJson(Map<String, dynamic> map) {
+    createdBy = map["createdBy"];
+    modifiedBy = map["modifiedBy"];
+    dateTimeCreated = map["dateTimeCreated"] == null
+        ? null
+        : DateTime.parse(map["dateTimeCreated"]);
+    dateTimeModified = map["dateTimeModified"] == null
+        ? null
+        : DateTime.parse(map["dateTimeModified"]);
+  }
+
+  Map<String, dynamic> toJson() => {
+        "createdBy": createdBy,
+        "modifiedBy": modifiedBy,
+        "dateTimeCreated": dateTimeCreated?.toString(),
+        "dateTimeModified": dateTimeCreated?.toString(),
+      };
 }
