6d845931e6ded25bd08e775a4b88ea2b02345f2b
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-03 16:45:29 +0500

    update to use base list card

--- Client/lib/views/pages/BranchView.dart
+++ Client/lib/views/pages/BranchView.dart
@@ -1,7 +1,8 @@
 import 'package:Client/models/BranchModel.dart';
+import 'package:Client/service/api/BranchApi.dart';
 import 'package:Client/viewmodels/BranchViewModel.dart';
-import 'package:Client/views/widgets/BaseForm/BaseForm.dart';
-import 'package:Client/views/widgets/Branch/BranchList.dart';
+import 'package:Client/views/widgets/Base/BaseForm.dart';
+import 'package:Client/views/widgets/Base/BaseListCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:provider/provider.dart';
@@ -16,26 +17,46 @@ class _BranchViewState extends State<BranchView> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<BranchViewModel>(
       create: (context) => BranchViewModel(),
-      builder: (context, child) => Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-              flex: 2,
-              child: Column(
-                children: [
-                  _form(),
-                  _form(isUpdateForm: true, context: context),
-                ],
-              )),
-          Expanded(flex: 5, child: BranchList())
-        ],
+      builder: (context, child) => Consumer<BranchViewModel>(
+        builder: (context, viewModel, child) => Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    _form(),
+                    _form(isUpdateForm: true, context: context),
+                  ],
+                )),
+            Expanded(
+              flex: 5,
+              child: viewModel.isFetchingData
+                  ? Center(child: CircularProgressIndicator())
+                  : BaseListCard(
+                      columns: [
+                        DataColumn(label: Text('Name')),
+                      ],
+                      rows: List.generate(
+                        viewModel.itemsList.length,
+                        (i) => DataRow(
+                            onSelectChanged: (_) => viewModel.selectItem(i),
+                            selected: viewModel.selectedItems[i],
+                            cells: [
+                              DataCell(Text(viewModel.itemsList[i].name)),
+                            ]),
+                      ).toList(),
+                    ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 _form({var isUpdateForm = false, var context}) {
-  return BaseForm<BranchViewModel>(
+  return BaseForm<BranchViewModel, BranchModel, BranchApi>(
     inputWidgets: [
       FormBuilderTextField(
         attribute: 'name',
