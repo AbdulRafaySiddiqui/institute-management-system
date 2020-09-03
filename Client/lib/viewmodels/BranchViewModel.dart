835087aae3b2ed1edc07128bd88a4b8b03813cd8
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-03 16:47:00 +0500

    moved api initialization to base class

--- Client/lib/viewmodels/BranchViewModel.dart
+++ Client/lib/viewmodels/BranchViewModel.dart
@@ -1,20 +1,8 @@
-import 'package:Client/locator/locator.dart';
 import 'package:Client/models/BranchModel.dart';
 import 'package:Client/service/api/BranchApi.dart';
-import 'package:Client/views/widgets/BaseForm/BaseItemViewModel.dart';
+import 'package:Client/views/widgets/Base/BaseItemViewModel.dart';
 
-class BranchViewModel extends BaseItemViewModel<BranchModel> {
-  BranchViewModel() {
-    api = locator<BranchApi>();
-    super.add = api.add;
-    super.update = api.update;
-    super.delete = api.delete;
-    super.fetch = api.fetchAll;
-    fetchAllItems();
-  }
-
-  var api;
-
+class BranchViewModel extends BaseItemViewModel<BranchModel, BranchApi> {
   @override
   BranchModel fromJson(Map<String, dynamic> map) {
     return BranchModel.fromJson(map);
