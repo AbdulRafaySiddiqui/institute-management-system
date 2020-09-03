2c85fc9aaf338c92f4cd4f187549941268936e3a
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-03 16:43:46 +0500

    added call to super constructor

--- Client/lib/models/BranchModel.dart
+++ Client/lib/models/BranchModel.dart
@@ -4,13 +4,17 @@ class BranchModel extends BaseModel {
   BranchModel({int id, this.name}) : super(id: id);
   String name;
 
-  BranchModel.fromJson(Map<String, dynamic> map) {
+  BranchModel.fromJson(Map<String, dynamic> map) : super.fromJson(map) {
     id = toInt(map['branchId']);
     name = map['name'];
   }
 
-  Map<String, dynamic> toJson() => {
-        "branchId": id,
-        "name": name,
-      };
+  Map<String, dynamic> toJson() {
+    var map = super.toJson();
+    map.addAll({
+      "branchId": id,
+      "name": name,
+    });
+    return map;
+  }
 }
