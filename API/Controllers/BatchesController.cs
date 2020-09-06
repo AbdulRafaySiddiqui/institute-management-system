db87a72426d349fd1bf4b74dc89cf14720bfdef9
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-06 21:49:41 +0500

    updated

--- API/Controllers/BatchesController.cs
+++ API/Controllers/BatchesController.cs
@@ -6,6 +6,6 @@ namespace API
     public class BatchesController : BaseController<Batch, IBatchService>
     {
         public BatchesController(IBatchService service)
-        : base(service: service, filterProp: e => e.ClassId ?? 0) { }
+        : base(service: service, filterProp: e => e.ClassId) { }
     }
 }
