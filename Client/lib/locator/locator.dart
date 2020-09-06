ac80e4e545b90580ac497999b40aa3217a189ff6
AbdulRafaySiddiqui <abdul.rafay.siddique@gmail.com>
2020-09-06 21:56:40 +0500

    added bachApi & currentBranchId

--- Client/lib/locator/locator.dart
+++ Client/lib/locator/locator.dart
@@ -1,4 +1,5 @@
 import 'package:Client/service/NavigationService/NavigationService.dart';
+import 'package:Client/service/api/BatchApi.dart';
 import 'package:Client/service/api/BranchApi.dart';
 import 'package:Client/service/api/ClassApi.dart';
 import 'package:get_it/get_it.dart';
@@ -9,4 +10,7 @@ void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => BranchApi());
   locator.registerLazySingleton(() => ClassApi());
+  locator.registerLazySingleton(() => BatchApi());
 }
+
+int get currentBranchId => 30;
